6e1308da9091ecf98a46f6903dddcb74eed5362c
bitacademy.bigdata.ai <bitacademy.bigdata.ai@gmail.com>
2022-11-14 14:53:44 +0900

    helloweb #3

--- helloweb03/src/main/java/com/bitacademy/helloweb/controller/HelloController.java
+++ helloweb03/src/main/java/com/bitacademy/helloweb/controller/HelloController.java
@@ -5,9 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
+	public void a() {
+	}
 	
 	@RequestMapping("/hello")
 	public String hello() {
 		return "/WEB-INF/views/hello.jsp";
 	}
+	
+	@RequestMapping("/hello2")
+	public String hello(String name) {
+		System.out.println("name:" + name + ", no:" + no);
+		return "/WEB-INF/views/hello.jsp";
+	}
+
+	
 }
